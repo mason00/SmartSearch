1dfece20ddc0dbf8fadc02aff019d7cc4327c3d9
Mason Hu <z1077908@wowcorp.com.au>
2022-03-19 14:02:16 +1100

    Add identity server

--- Woolworths.Groot.SmartSearch/Woolworths.Groot.SmartSearch/Program.cs
+++ Woolworths.Groot.SmartSearch/Woolworths.Groot.SmartSearch/Program.cs
@@ -1,3 +1,5 @@
+using Duende.IdentityServer;
+using IdentityServerHost;
 using Woolworths.Groot.SmartSearch.MongoDb;
 using Woolworths.Groot.SmartSearch.Services;
 
@@ -29,6 +31,48 @@ builder.Services.AddCors(options =>
     });
 });
 
+var identityBuilder = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+
+    // see https://docs.duendesoftware.com/identityserver/v5/basics/resources
+    options.EmitStaticAudienceClaim = true;
+})
+                .AddTestUsers(TestUsers.Users)
+                ;
+
+identityBuilder.AddInMemoryIdentityResources(Resources.Identity);
+identityBuilder.AddInMemoryApiScopes(Resources.ApiScopes);
+identityBuilder.AddInMemoryApiResources(Resources.ApiResources);
+identityBuilder.AddInMemoryClients(Clients.List);
+
+//// this is only needed for the JAR and JWT samples and adds supports for JWT-based client authentication
+//identityBuilder.AddJwtBearerClientAuthentication();
+
+builder.Services.AddAuthentication()
+    .AddOpenIdConnect("Google", "Sign-in with Google", options =>
+    {
+        options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+        options.ForwardSignOut = IdentityServerConstants.DefaultCookieAuthenticationScheme;
+
+        options.Authority = "https://accounts.google.com/";
+        options.ClientId = "535648816304-3euqfipgu7tbje7re64j9iqmskalsit3.apps.googleusercontent.com";
+        options.ClientSecret = "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";
+
+        options.CallbackPath = "/signin-google";
+        options.Scope.Add("email");
+    });
+//.AddGoogle("Google", options =>
+//{
+//    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+//    options.ClientId = "535648816304-3euqfipgu7tbje7re64j9iqmskalsit3.apps.googleusercontent.com";
+//    options.ClientSecret = "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";
+//});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -36,12 +80,16 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseCors();
 
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
